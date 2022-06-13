11a23284cd54be2c4dfbf1e582e2b317fc048e64
Alvise Dabalà <82535389+AlviDab@users.noreply.github.com>
2022-06-13 16:16:44 +0200

    Cleaning up code
    Removing parenthesis

--- 09_SpatPlan_WestPac_nMDS.R
+++ 09_SpatPlan_WestPac_nMDS.R
@@ -66,22 +66,22 @@ plotOrdination <- function(x, palette) {
 
 # Scenario Theme
 palette <- c("SSP1-2.6" = "#289E3D", "SSP2-4.5" = "#E6C173", "SSP5-8.5" = "#855600")
-(plot <- plotOrdination("scenario", palette))
+plot <- plotOrdination("scenario", palette)
 ggsave("Figures/nMDS_scenario.png", plot = plot, height = 10, width = 10, dpi = 600)
 
 # Ensemble Theme
 palette <- c("EM" = "#FAF7B7", "CanESM5" = "#E6C173", "CMCC-ESM2" = "#855600", "GFDL-ESM4" = "#5075BA", "IPSL-CM6A-LR" = "#81B0CC", "NorESM2-MM" = "#5A9E67")
-(plot <- plotOrdination("model", palette))
+plot <- plotOrdination("model", palette)
 ggsave("Figures/nMDS_ensemble.png", plot = plot, height = 10, width = 10, dpi = 600)
 
 # Metrics theme
 palette <- c("MHW_SumCumInt" = "#3C6342", "o2os" = "#289E3D", "phos" = "#E6C173", "tos" = "#81B0CC", "velocity" = "#855600")
-(plot <- plotOrdination("metric", palette))
+plot <- plotOrdination("metric", palette)
 ggsave("Figures/nmds_metric.png", plot = plot, height = 10, width = 10, dpi = 600)
 
 # Approach Theme
 palette <- c("climate priority area" = "#E6BA7E", "feature" = "#4D3B2A", "penalty" = "#6984BF", "percentile" = "#2B8142")
-(plot <- plotOrdination("approach", palette))
+plot <- plotOrdination("approach", palette)
 ggsave("Figures/nmds_approach.png", plot = plot, height = 10, width = 10, dpi = 600)
 
 
