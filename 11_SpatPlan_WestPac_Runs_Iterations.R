8f09125a262c8196670ea516344a055898489607
Tin Buenafe <78692266+tinbuenafe@users.noreply.github.com>
2022-12-11 22:45:02 +0800

    Final CPA runs

--- 11_SpatPlan_WestPac_Runs_Iterations.R
+++ 11_SpatPlan_WestPac_Runs_Iterations.R
@@ -9,20 +9,10 @@
 source("03_SpatPlan_Master_Preliminaries.R") # climate layers are loaded in the script
 
 # Loop through climate priority area
-solution_list <- paste0("s", seq(285, 289, 1))
+solution_list <- paste0("s", seq(287, 289, 1))
 metric_list <- c("velocity")
 scenario_list <- c("SSP 5-8.5")
-model_list <- c("CanESM5", "CMCC-ESM2", "GFDL-ESM4", "IPSL-CM6A-LR", "NorESM2-MM")
-
-system.time({loopthrough_CPA(solution_list, 
-                             metric_list, 
-                             scenario_list, 
-                             model_list)})
-
-solution_list <- paste0("s", seq(419, 433, 1))
-metric_list <- c("CombinedMetric")
-scenario_list <- c("SSP 1-2.6", "SSP 2-4.5", "SSP 5-8.5")
-model_list <- c("CanESM5", "CMCC-ESM2", "GFDL-ESM4", "IPSL-CM6A-LR", "NorESM2-MM")
+model_list <- c("GFDL-ESM4", "IPSL-CM6A-LR", "NorESM2-MM")
 
 system.time({loopthrough_CPA(solution_list, 
                              metric_list, 
